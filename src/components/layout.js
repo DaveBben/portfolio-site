d40bbdd238953502fa7e42ae194d6d9c7795f0d6
Dave Bennett <david.a.bennett7@gmail.com>
2019-11-02 09:20:48 -0400

    Initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./index.scss"
+import Nav from "./nav/Nav"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,21 +26,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <div className="app-container">
+        <div className="nav-container">
+          <Nav />
+        </div>
+        <main className="main-content">{children}</main>
       </div>
     </>
   )
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,14 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Nav = () => (
+  <div className="navigation">
+    <div class="section"></div>
+    <div class="section"></div>
+    <div class="section"></div>
+    <div class="section"></div>
+  </div>
+)
+
+export default Nav
